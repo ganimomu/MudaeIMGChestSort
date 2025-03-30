2ad60b4f12413652128cb874de45be94a40a6632
Gabriel Moreno <ganimomu2001@gmail.com>
2025-03-30 15:54:04 -0300

    final update

--- app.js
+++ app.js
@@ -1,61 +1,83 @@
 let token;
+if (localStorage.getItem("apiKey")) {
+    token = localStorage.getItem("apiKey");
+    document.getElementById("txtApi").value = token;
+}
 let grabData;
 let url = "https://api.imgchest.com/v1/post/"
+let output = "";
+const toast = document.getElementById('liveToast');
+const toastText = document.getElementById('toastBody');
 
 document.getElementById("txtApi").addEventListener("change", () => {
     token = document.getElementById("txtApi").value.trim(); console.log(token);
 })
 document.getElementById("btnGrab").addEventListener("click", grab);
+document.getElementById("btnCopy").addEventListener("click", copy);
 
 function copy() {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
     let box = document.getElementById("txtOut");
 
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-
+    box.select();
+    box.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(box.value);
+    toastText.innerHTML = "Text copied to clipboard";
+    toastBootstrap.show()
 }
 
 function grab() {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
     let albumField = document.getElementById("txtLink");
-    if(!token) {
-        return alert("Token not set");
+    if (!token) {
+        toastText.innerHTML = "API Token not set";
+        toastBootstrap.show()
+        return
     }
     if (!albumField.value.trim()) {
-        return alert("Album URL not set");
+        toastText.innerHTML = "Album URL not set";
+        toastBootstrap.show()
+        return
     }
     let albumId = albumField.value;
     console.log(albumId);
-    
-    if(albumId.indexOf("h") == 0) {
+
+    if (albumId.indexOf("h") == 0) {
         albumId = albumId.slice(23)
-    } else {        
-        albumId= albumId.slice(15)
+    } else {
+        albumId = albumId.slice(15)
     }
     console.log(albumId);
-    
-    fetch(url+albumId, {
+
+    fetch(url + albumId, {
+        mode: "cors",
         headers: {
-            "Authorization": token,
-            "Access-Control-Request-Method": "GET, OPTIONS",
-            "ccess-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin": "origin",
-            "Access-Control-Allow-Credentials": true
+            "Authorization": "Bearer " + token
         }
     }).then(response => {
-        if(!response.ok){
-            console.log(response);            
+        if (!response.ok) {
+            console.log(response);
             return alert("Something went wrong.")
+        }
+        if (response.ok) {
+            localStorage.setItem("apiKey", token);
         }
         return response.json();
     }).then(data => {
         console.log(data);
         grabData = data;
+        grabData.data.images.forEach(element => {
+            if (element) {
+
+                output += element.link + " $ ";
+            }
+        });
+        output = output.slice(0, output.length - 3);
+        document.getElementById("txtOut").value = output;
+        document.getElementById("divCopy").removeAttribute("hidden");
     }).catch(error => {
-        console.log("Error: ", error);        
+        console.log("Error: ", error);
     })
 
-    console.log(grabData);
-    
 
 }
